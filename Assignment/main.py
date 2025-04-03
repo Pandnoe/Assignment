48172249adba52246f55cde9098a3663415f15bb
Louiss <poopingmushes29@gmail.com>
2025-04-03 19:52:36 +0800

    Update main.py

--- Assignment/main.py
+++ Assignment/main.py
@@ -83,6 +83,10 @@ if not st.session_state.logged_in:
         if st.button("Register"):
             if reg_password != reg_password_confirm:
                 st.error("Passwords do not match!")
+            elif reg_password == "None":
+                st.error("Enter a password!")
+            elif reg_username == "None":
+                st.error("Enter a username!")
             else:
                 success, message = register_user(reg_username, reg_password)
                 if success:
